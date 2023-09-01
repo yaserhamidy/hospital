b03d612d45208fb0a8e14f71166de02239694fd3
yaser <yaserhamidy1212@gmail.com>
2023-09-01 01:48:57 -0700

    some change

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hospital/splash_screen.dart';
 
 main(){
-  
   runApp(
-
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
